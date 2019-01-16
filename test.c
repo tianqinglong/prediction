273338ca7c8a7ceb758c6e902baafd5fb29244b1
qltian <qltian@smaster.stat.iastate.edu>
2019-01-15 23:58:55 -0600

    a.out

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+     #include <time.h>
+     #include <stdlib.h>
+     int main() {
+       int i;
+       srand(time(NULL)); /* set seed */
+       for (i=1; i<=10; i++){
+         printf("%f ", rand() /  (double) RAND_MAX );
+}
+return 0; }
