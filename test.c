eac44020f6aa7edce1e6eb4eb046856cb0101a15
Qinglong Tian - student <qltian@condo2017.its.iastate.edu>
2019-01-16 15:43:11 -0600

    test again

--- test.c
+++ test.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 int main() {
     int i;
-       srand(time(NULL)); /* set seed */
-       for (i=1; i<=10; i++){
+    srand(time(NULL)); /* set seed */
+    for (i=1; i<=10; i++){
          printf("%f ", rand() /  (double) RAND_MAX );
 }
 return 0; }
