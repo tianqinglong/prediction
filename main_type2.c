b8aef95fc7a37189336c9a0ba93b6e010007a3e7
Qinglong <qltian@iastate.edu>
2019-01-23 22:18:37 -0600

    Generate Bootstrap and MLE under Type 2 Censoring

--- main_type2.c
+++ main_type2.c
@@ -8,14 +8,14 @@
 
 int r=30, n=50;
 double weiEta=1.5, weiBeta=1.5;
-double cenArray[ARRAY_MAX], realArray[ARRAY_MAX];
+double cenArray[ARRAY_MAX], realArray[ARRAY_MAX], weightArray[ARRAY_MAX];
 
 int main(){
 	int i;
-	double betaMLE, etaMLE;
-	double t, machep;
+	double betaMLE, etaMLE, betaMLEBoot, etaMLEBoot;
+	double t, machep, nt, dt;
 
-	set_seed(time(NULL),123);
+	set_seed(time(NULL),500);
 	simuDataType2(cenArray, realArray);
 
 	machep = r8_epsilon();
@@ -30,6 +30,20 @@ int main(){
 	etaMLE = pow(etaMLE/r,1/betaMLE);
 
 	printf("%f %f\n", betaMLE, etaMLE);
+
+	getWeight(weightArray);
+	/*
+	for(i=1;i<n;i++){
+		printf("%f\n", weightArray[i]);
+	}*/
+	betaMLEBoot = zero(0.1,10,machep,t,deriFRWB);
+	nt=0;dt=0;
+	for(i=0;i<n;i++){
+		nt += weightArray[i]*pow(cenArray[i],betaMLEBoot);
+		dt += ((i<r) ? weightArray[i] : 0);
+	}
+	etaMLEBoot = pow(nt/dt, 1/betaMLEBoot);
+	printf("%f %f\n", betaMLEBoot, etaMLEBoot);
 
 	return 0;
 }
--- overall_type2.h
+++ overall_type2.h
@@ -2,7 +2,9 @@
 
 extern int n, r;
 extern double weiEta, weiBeta;
-extern double cenArray[], realArray[];
+extern double cenArray[], realArray[], weightArray[];
 
 void simuDataType2(double* cenVec, double* comVec);
 double deriEquation(double weibullBeta);
+double deriFRWB(double weibullBeta);
+void getWeight(double *weight);
